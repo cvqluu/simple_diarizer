aa8731b8bee64b98e18342a09d85d09a87d36068
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2022-01-10 15:55:19 -0300

    Improvements long_description

--- setup.py
+++ setup.py
@@ -2,6 +2,8 @@ import setuptools
 
 from simple_diarizer import __version__
 
+with open("README.md", "r") as f:
+    long_description = f.read()
 
 with open("requirements.txt", "r", encoding="utf-8") as f:
     install_requires = f.read()
