77cd681ad509b569e683a6b6e1d4204a0462cfca
Johni Douglas Marangon <johni.douglas.marangon@gmail.com>
2022-01-10 15:55:19 -0300

    Improvements long_description

--- setup.py
+++ setup.py
@@ -3,10 +3,14 @@ import setuptools
 from simple_diarizer import __version__
 
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read(
+
 with open("requirements.txt", "r", encoding="utf-8") as f:
     install_requires = f.read()
 
 setuptools.setup(
     version=__version__,
+    long_description=long_description,
     install_requires=install_requires,
 )
